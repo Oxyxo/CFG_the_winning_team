d4527c8bdd2c3238a85bea569fbac3a0a4f92b3c
candywee <idkanm4@gmail.com>
2022-11-01 22:25:42 +0100

    added looping through the questions and quit option

--- main.py
+++ main.py
@@ -1,55 +1,61 @@
 import requests
 from datetime import datetime
 
-def get_genres():
-    response = requests.get(
-        f"https://api.themoviedb.org/3/genre/movie/list?api_key=91b59d00685f5ec6b4534a4b11ae1ffb")
-    data = response.json()
-    genres = {}
-    for genre in data["genres"]:
-        genres[genre["id"]] = genre["name"]
-    return genres
-
-genres = get_genres()
-
-def print_movie_result(movie):
-    date = datetime.strptime(movie['release_date'], "%Y-%m-%d")
-    genres_movie = []
-    for id in movie["genre_ids"]:
-        genres_movie.append(genres[id])
-    print(movie['original_title']+" - "+ str(date.year))
-    print(', '.join(genres_movie))
-
-
-user_movie = input("Which movie are you looking for?\n")
-title_with_hyphen = user_movie.replace(" ", "-")
-
-response_API = requests.get(
-    f"https://api.themoviedb.org/3/search/movie?api_key=91b59d00685f5ec6b4534a4b11ae1ffb&page=1&query={title_with_hyphen}")
-data = response_API.json()
-
-movie_list = []
-movie_id = 0
-list_number = 1
-
-for movie in data["results"]:
-    movie_list.append(f"{list_number}. " + data["results"][movie_id]["original_title"])
-    movie_id += 1
-    list_number += 1
-print("\n".join(movie_list))
-
-for movie in data["results"]:
-    try:
-        print_movie_result(movie)
-    except:
-        continue
+# def get_genres():
+#     response = requests.get(
+#         f"https://api.themoviedb.org/3/genre/movie/list?api_key=91b59d00685f5ec6b4534a4b11ae1ffb")
+#     data = response.json()
+#     genres = {}
+#     for genre in data["genres"]:
+#         genres[genre["id"]] = genre["name"]
+#     return genres
+#
+# genres = get_genres()
+#
+# def print_movie_result(movie):
+#     date = datetime.strptime(movie['release_date'], "%Y-%m-%d")
+#     genres_movie = []
+#     for id in movie["genre_ids"]:
+#         genres_movie.append(genres[id])
+#     print(movie['original_title']+" - "+ str(date.year))
+#     print(', '.join(genres_movie))
 
 
+while True:
+    user_movie = input("Which movie are you looking for?\n")
+    if user_movie == ":q":
+        break
 
-overview_for_chosen = input("About which movie would you like to read? Give me a number from the list^\n")
-number = int(overview_for_chosen) - 1
+    title_with_hyphen = user_movie.replace(" ", "-")
 
-print(data["results"][number]["original_title"] +" "+ data["results"][number]["release_date"][0:4])
-print(data["results"][number]["overview"])
+    response_API = requests.get(
+        f"https://api.themoviedb.org/3/search/movie?api_key=91b59d00685f5ec6b4534a4b11ae1ffb&page=1&query={title_with_hyphen}")
+    data = response_API.json()
+
+    movie_list = []
+    movie_id = 0
+    list_number = 1
+
+    for movie in data["results"]:
+        movie_list.append(f"{list_number}. " + data["results"][movie_id]["original_title"])
+        movie_id += 1
+        list_number += 1
+    print("\n".join(movie_list))
+
+    for movie in data["results"]:
+        try:
+            print_movie_result(movie)
+        except:
+            continue
+
+
+
+    overview_for_chosen = input("About which movie would you like to read? Give me a number from the list^\n")
+    if overview_for_chosen == ":q":
+        break
+
+    number = int(overview_for_chosen) - 1
+    print(data["results"][number]["original_title"] +" "+ data["results"][number]["release_date"][0:4])
+    print(data["results"][number]["overview"])
 
 
