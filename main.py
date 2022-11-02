927e0c74c2c0a4d802ce95ecce6763026d6812cb
Oxana Rinzema-Bulanova <oxana_bulanova@hotmail.com>
2022-11-02 16:41:04 +0100

    expanding search
    added search by number 1-20 of the previously found movie. If user inputs wrong number or symbole the program stops. User can type exit to quit the loop.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 
+#part 1
 def get_genres():
     response = requests.get(
         f"https://api.themoviedb.org/3/genre/movie/list?api_key=91b59d00685f5ec6b4534a4b11ae1ffb")
@@ -12,35 +13,57 @@ def get_genres():
 
 genres = get_genres()
 
-def print_movie_result(movie):
+def print_movie_result(number, movie):
     date = datetime.strptime(movie['release_date'], "%Y-%m-%d")
     genres_movie = []
     for id in movie["genre_ids"]:
         genres_movie.append(genres[id])
-    print(movie['original_title']+" - "+ str(date.year))
+    print(str(number) +". "+ movie['original_title']+" - "+ str(date.year))
     print(', '.join(genres_movie))
 
+while True:
+    user_movie = input("Which movie are you looking for? (type exit to quit)\n")
+    if user_movie == "exit":
+        break
+    title_with_hyphen = user_movie.replace(" ", "-")
 
-user_movie = input("Which movie are you looking for?\n")
-title_with_hyphen = user_movie.replace(" ", "-")
+    response_API = requests.get(
+        f"https://api.themoviedb.org/3/search/movie?api_key=91b59d00685f5ec6b4534a4b11ae1ffb&page=1&query={title_with_hyphen}")
+    data = response_API.json()
 
-response_API = requests.get(
-    f"https://api.themoviedb.org/3/search/movie?api_key=91b59d00685f5ec6b4534a4b11ae1ffb&page=1&query={title_with_hyphen}")
-data = response_API.json()
+    movies = data["results"]
+
+    for number, movie in enumerate(movies):
+        number = number + 1
+        try:
+            print_movie_result(number, movie)
+        except:
+            continue
 
 
-for movie in data["results"]:
-    try:
-        print_movie_result(movie)
-    except:
-        continue
+    #part 2 additional information about movie: overview and score
+    def print_additional_data(movie):
+        print(movie["original_title"] +": " +movie["overview"])
+        if movie["vote_average"] >= 6.00:
+            print("This movie is popular!")
+        elif 0 < movie["vote_average"] < 6.00:
+            print("This movie scored below average on reviews")
+        else:
+            return
 
-overview_for_chosen = input("About which movie would you like to read?\n")
-print(overview_for_chosen)
-hyphen = overview_for_chosen.replace(" ", "-")
-from_API = requests.get(
-    f"https://api.themoviedb.org/3/search/movie?api_key=91b59d00685f5ec6b4534a4b11ae1ffb&page=1&query={hyphen}")
-print(data["results"][0]["overview"])
+    overview_for_chosen = input("About which movie would you like to read? Please enter a number between 1-20.\n")
+    if overview_for_chosen.isdigit():
+        if 1<= int(overview_for_chosen)-1<=20:
+            selected_movie = movies[int(overview_for_chosen)-1]
+        else:
+            print("Error, number is out of range")
+            break
+    else:
+        print("Error, wrong symbol was entered!")
+        break
+
+    #this prints the selected movie title, overview and score
+    print_additional_data(selected_movie)
 
 
 
